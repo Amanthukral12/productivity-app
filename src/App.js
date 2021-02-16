110ccbbc55ed34af991794e6346b3109c20a1520
Aman Thukral <thukral618@gmail.com>
2021-02-16 17:01:22 +0530

    Adding firebase support for signin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Home
+    
+    Login  
+    Home
+    Todo
+    Notes
+    Drive
+    Movie
     </div>
   );
 }
