7400eb62b9644944d2d0fbc758bcf1a498582c39
Aman Thukral <thukral618@gmail.com>
2021-03-22 04:25:33 +0530

    improving responsiveness in the dashboard page

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useHistory, Link } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import Quotes from './Components/Quotes/Quotes';
-import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
-import NoteAddIcon from '@material-ui/icons/NoteAdd';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import Fade from '@material-ui/core/Fade';
@@ -40,9 +38,9 @@ const Dashboard = () => {
         let hours = today.getHours();
         setHours(hours);
         if(hours > 12 && hours<16){
-            setMessage("Good Afternoon")
+            setMessage("Good Afternoon!")
         } else if(hours > 4 && hours < 12){
-            setMessage("Good Morning")
+            setMessage("Good Morning!")
         } else {
             setMessage("Good Evening!")
         } 
@@ -66,7 +64,7 @@ const Dashboard = () => {
 
     return (
         <div className="root" >
-            <div style={{display: "flex", justifyContent: "space-between"}}>
+            <div style={{display: "flex", justifyContent: "space-between", zIndex:3, }}>
             <Drawer
 						open={isOpen}
 						onClose={() => setIsOpen(false)}
@@ -75,17 +73,15 @@ const Dashboard = () => {
 						PaperProps={{
 						  style: {
 							width: "30vh",
-                            /* background: "linear-gradient(to top, rgba(145, 178, 248, 0.3), rgba(145, 178, 248, 0.1))", */
                             backgroundColor: "rgba(255, 255, 255, 0.4)",
-                            backdropFilter: "blur(4px)"
-                            
+                            backdropFilter: "blur(4px)" 
 						  },
 						}}
 					  >
 						<Sidebar />
 					  </Drawer>	
             <MenuIcon onClick={() => setIsOpen(true)} className="menuIcon" />
-                <h1>App Name</h1>
+                <h1 className="appName">Producto</h1>
                 <img src={currentUser.photoURL}
                       alt="Profile Image"
                       onClick={handleClick}
@@ -98,39 +94,47 @@ const Dashboard = () => {
                     onClose={handleClose}
                     TransitionComponent={Fade}
                 >
+                    <div className="profileMenuHead">
+                        <img src={currentUser.photoURL}
+                            alt="Profile Image"
+                            onClick={handleClick}
+                            className="menuProfileImage"
+                        />
+                        <h2>{currentUser.displayName}</h2>
+                        <h3>{currentUser.email}</h3>
+                    </div>
+                    <hr />
                     <MenuItem style={{width: "300px"}} onClick={handleClose}><Link to="/update-profile">Update Profile</Link></MenuItem>
                     <MenuItem onClick={handleLogout}>Logout</MenuItem>
                     
-                </Menu>
-                
-                {/* <div onClick={handleLogout} style={{width: "50px", cursor:"pointer"}}>Logout</div> */}
+                </Menu> 
             </div>
-            <div className="backgroundTile"></div>
+            <div class="custom-shape-divider-top-1616160906">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+                        <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+                        <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+                    </svg>
+            </div>
+            {/* <div className="backgroundTile"></div> */}
+            
             <h1 className="time">{hour}</h1>
             <div className="greeting">{message}</div>
             <div className="name">{currentUser.displayName}</div>
-            
-            {/* Name */}
-            
-            
-            {/* todo */}
-            
-
-            {error && <h1>{error}</h1>}
-            
-            
-            
+                {error && <h1>{error}</h1>}
             <div>
-           
-            {/* <Link to="/update-profile">Update Profile</Link>
-            <br />
-            <Link to="/todo-list">Todo List</Link>
-            <br/>
-            <Link to="/notes-app">Notes App</Link> */}
         </div>
-        
-        
         <Quotes />
+        
+            <div class="custom-shape-divider-bottom-1616160906">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+                        <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+                        <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+                    </svg>
+            </div>
+ 
+
         </div>
     )
 }
