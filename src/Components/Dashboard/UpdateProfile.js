2830ebed634b77cccf9a2353c343873cafd7b49c
Aman Thukral <thukral618@gmail.com>
2021-04-21 17:34:37 +0530

    Adding update profile styling

--- src/Components/Dashboard/UpdateProfile.js
+++ src/Components/Dashboard/UpdateProfile.js
@@ -4,6 +4,8 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import { useAuth } from '../../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom';
+import "../LoginLogout/Login.css";
+import photo from '../LoginLogout/photo1.jpg'
 const UpdateProfile = () => {
     const nameRef = useRef();
     const emailRef = useRef();
@@ -48,11 +50,16 @@ const UpdateProfile = () => {
         
     }
     return (
-        <>
-           <Grid>
-            <h2>Update Profile</h2>
-            {error && <h1>{error}</h1>}
-            <form onSubmit={handleSubmit}>
+           <Grid className="loginRoot">
+           <div className="ground">
+            <div className="loginPhoto">
+                <img src={photo} className="loginImage" alt=""/>
+            </div>
+            <div className="loginRight">
+            <h2 className="loginHeading">Produkto</h2>
+            {error && <h1 className="error">{error}</h1>}
+            <h3 className="welcomeMessage">Update Profile</h3>           
+            <form onSubmit={handleSubmit} className="loginForm">
             <TextField label='Name' placeholder='Enter Name' type='text' inputRef={nameRef} required defaultValue={currentUser.displayName} />
             <br/>
             <TextField label='Email' placeholder='Enter Email' type='email' inputRef={emailRef} required defaultValue={currentUser.email} />
@@ -61,11 +68,13 @@ const UpdateProfile = () => {
             <br/>
             <TextField label='Confirm Password' placeholder='Leave Blank to keep it same' type='password' inputRef={passwordConfirmRef} />
             <br/>
-            <Button disabled={loading} type="submit">Update Profile</Button>
+            <button disabled={loading} type="submit" className="signin">Update Profile</button>
             </form>
-            <h3><Link to="/">Cancel</Link></h3>
+            <h3 ><Link to="/" className="newUser">Cancel</Link></h3>
+            </div>
+            </div>
+            
            </Grid>
-        </>
     )
 }
 
