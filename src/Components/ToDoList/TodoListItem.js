fa1fc9aa3c1f0e2adec2f7993d64b3d1eec82898
Aman Thukral <thukral618@gmail.com>
2022-01-07 01:49:19 +0530

    Removing unused imports

--- src/Components/ToDoList/TodoListItem.js
+++ src/Components/ToDoList/TodoListItem.js
@@ -1,16 +1,9 @@
 import React from 'react'
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import { db } from '../../firebase';
 import "./TodoListItem.css"
 import DeleteIcon from '@material-ui/icons/Delete';
 const TodoListItem = ({todo, inprogress, id, currentUser}) => {
 
-    const toggleInProgress = () => {
-        db.collection("users").doc(currentUser.uid).collection("todos").doc(id).update({
-            inprogress: !inprogress
-        })
-    }
 
     const deleteTodo = () => {
         db.collection("users").doc(currentUser.uid).collection("todos").doc(id).delete();
