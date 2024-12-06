f698a2c071809198bffc0f1906bfc84d79caa9d3
Aman Thukral <thukral618@gmail.com>
2024-12-06 15:00:14 +0530

    Fresh start from React+Firebase to Full Stack

--- src/Components/LoginLogout/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { currentUser } = useAuth();
-  return currentUser ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
--- src/Components/LoginLogout/PrivateRoute2.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { currentUser } = useAuth();
-  return currentUser === null ? <Outlet /> : <Navigate to="/" replace />;
-};
-
-export default PrivateRoute;
