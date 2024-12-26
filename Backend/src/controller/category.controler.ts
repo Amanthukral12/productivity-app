b66e083a8bcd944debcb6a36c8c70f6eef254592
Aman Thukral <thukral618@gmail.com>
2024-12-26 08:48:26 +0530

    Add getCategories and deleteCategories controller functions

--- Backend/src/controller/category.controler.ts
+++ Backend/src/controller/category.controler.ts
@@ -7,53 +7,105 @@ import { ApiResponse } from "../utils/ApiResponse";
 
 export const createCategory = asyncHandler(
   async (req: Request, res: Response) => {
-    try {
-      if (!req.user) {
-        throw new ApiError(401, "Unauthorized Access. Please login again.", [
-          "Unauthorized Access. Please login again.",
-        ]);
-      }
-      const userId = (req.user as UserDocument).id;
-
-      const { name } = req.body;
-      if (!name) {
-        return res
-          .status(400)
-          .json(new ApiResponse(400, {}, "Category name is required"));
-      }
-
-      const catefogyExists = await prisma.category.findFirst({
-        where: {
-          name,
-          userId,
-        },
-      });
-
-      if (catefogyExists) {
-        return res
-          .status(400)
-          .json(new ApiResponse(400, {}, "Category already exists"));
-      }
-
-      const category = await prisma.category.create({
-        data: {
-          name,
-          userId,
-        },
-      });
-      res
-        .status(201)
-        .json(
-          new ApiResponse(
-            201,
-            category,
-            `${category.name} created successfully`
-          )
-        );
-    } catch (error) {
-      throw new ApiError(400, "Error creating category", [
-        "Error creating category",
+    if (!req.user) {
+      throw new ApiError(401, "Unauthorized Access. Please login again.", [
+        "Unauthorized Access. Please login again.",
       ]);
     }
+    const userId = (req.user as UserDocument).id;
+
+    const { name } = req.body;
+    if (!name) {
+      throw new ApiError(400, "Category name is required", [
+        "Category name is required",
+      ]);
+    }
+
+    const catefogyExists = await prisma.category.findFirst({
+      where: {
+        name,
+        userId,
+      },
+    });
+
+    if (catefogyExists) {
+      throw new ApiError(400, "Category already exists", [
+        "Category already exists",
+      ]);
+    }
+
+    const category = await prisma.category.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+    res
+      .status(201)
+      .json(
+        new ApiResponse(201, category, `${category.name} created successfully`)
+      );
+  }
+);
+
+export const deleteCategory = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.user) {
+      throw new ApiError(401, "Unauthorized Access. Please login again.", [
+        "Unauthorized Access. Please login again.",
+      ]);
+    }
+    const userId = (req.user as UserDocument).id;
+
+    const { categoryId } = req.params;
+
+    const categoryExists = await prisma.category.findFirst({
+      where: {
+        id: Number(categoryId),
+        userId,
+      },
+    });
+
+    if (!categoryExists) {
+      throw new ApiError(404, "Category not found", ["Category not found"]);
+    }
+
+    await prisma.category.delete({
+      where: {
+        id: Number(categoryId),
+      },
+    });
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          {},
+          `${categoryExists.name} category deleted successfully`
+        )
+      );
+  }
+);
+
+export const getCategories = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.user) {
+      throw new ApiError(401, "Unauthorized Access. Please login again.", [
+        "Unauthorized Access. Please login again.",
+      ]);
+    }
+    const userId = (req.user as UserDocument).id;
+
+    const categories = await prisma.category.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, categories, "Categories fetched successfully")
+      );
   }
 );
--- Backend/src/routes/category.routes.ts
+++ Backend/src/routes/category.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { authenticateSession } from "../middleware/auth";
-import { createCategory } from "../controller/category.controler";
+import {
+  createCategory,
+  deleteCategory,
+  getCategories,
+} from "../controller/category.controler";
 
 const router = Router();
 
 router.route("/add").post(authenticateSession, createCategory);
+router.route("/:categoryId").delete(authenticateSession, deleteCategory);
+router.route("/").get(authenticateSession, getCategories);
 
 export default router;
