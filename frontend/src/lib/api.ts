7cdde585784b3fdd3494e96e92ccbe38c4906ca9
Aman Thukral <thukral618@gmail.com>
2025-01-21 03:22:50 +0530

    Add the feature to automatically refresh access token

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/",
+  withCredentials: true,
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const response = await refreshToken();
+        const { accessToken } = response.data;
+        localStorage.setItem("token", accessToken);
+        api.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+        originalRequest.headers["Authorization"] = `Bearer ${accessToken}`;
+        return api(originalRequest);
+      } catch (refreshError) {
+        console.log(refreshError);
+        localStorage.clear();
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
+
+async function refreshToken() {
+  try {
+    const res = await axios.post(
+      "/auth/refresh-token",
+      {},
+      { withCredentials: true }
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
